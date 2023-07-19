7bf2e300f30fe4ea87ed61e4b45b94e3cca34471
Gabe Glasco <gabeglasco@gmail.com>
2023-07-19 14:10:39 -0500

    added base requirements

--- server/public/client.js
+++ server/public/client.js
@@ -2,11 +2,19 @@ $(document).ready(onReady);
 
 function onReady() {
     $.ajax({
+        // Tells Ajax (our jscript tool which allows our client to
+        // communicate with nour server) 
+        //  how to to communicate with our server.
         type: 'GET',
+        // .... and where to go to get it... the route!
         url: '/artist'
+        // .then is a function that will take in our response from the server
+        // and pass it as an argument
     }).then(function (response) {
+        // while looping over our array of artists
         for (let i = 0; i < response.length; i++) {
             let artist = response[i];
+            // which will append it to our DOM
             $('#artistTableBody').append(`
                 <tr>
                     <td>${artist.name}</td>
@@ -17,5 +25,25 @@ function onReady() {
         }
     });
 
+    $.ajax({
+        
+        type: 'GET',
+        
+        url: '/song'
+        
+    }).then(function (response) {
+        
+        for (let i = 0; i < response.length; i++) {
+            let song = response[i];
+            
+            $('#songTableBody').append(`
+                <tr>
+                    
+                    <td>${song.title}</td>
+                    <td>${song.artist}</td>
+                </tr>
+            `);
+        }
+    });
     // TODO Add ajax request for /songs and display on DOM
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+// We declare express and request access to express for functionality in the server
 const express = require('express');
 
+// We are declaring app have the functionality of express
 const app = express();
+// We are declaring that we will allow it to run on our local host at port 5000
 const PORT = 5000;
 
+// This is the array that contains objects of artists that lives on our server
 const artistListArray = [
     {
         name: 'Miles Davis',
@@ -26,6 +30,7 @@ const artistListArray = [
     },
 ];
 
+// This is the array of song objects that lives on our server
 const songListArray = [
     {
         title: 'Take Five',
@@ -45,14 +50,26 @@ const songListArray = [
     },
 ];
 
+// This tells express to deliver static code from our server to the DOM
+// gives access to files in our server at the public folder
 app.use(express.static('server/public'));
 
+// Creates the GET route
+// Path is /artist
+// Also has an anonymous function with two parameters, 
+// req and res (request and response)
 app.get('/artist', (req, res) => {
+    
+    // The server will respond with the array of artists
     res.send(artistListArray);
 });
 
 // TODO - Add GET for songs
-
+app.get('/song', (req, res)=>{
+    res.send(songListArray);
+})
+// Instructs express to listen to the port we established
+// This is our port binding
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
